fbd646220d29c1964c189d0d3be07d4e07f4e4e3
Orosz Sándor <sandor.orosz@icellmobilsoft.hu>
2023-04-18 23:34:57 +0200

    Ident that JSON

--- src/main/java/jackson/ObjectMapperProducer.java
+++ src/main/java/jackson/ObjectMapperProducer.java
@@ -1,6 +1,8 @@
 package jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import jakarta.enterprise.inject.Produces;
 
@@ -8,7 +10,7 @@ public class ObjectMapperProducer {
 
     @Produces
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return JsonMapper.builder().enable(SerializationFeature.INDENT_OUTPUT).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS).build();
     }
 
 }
